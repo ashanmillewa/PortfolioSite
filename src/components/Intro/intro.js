70cfa7b53e05d11e2d5b78e8c517ac5c3d0c3a4f
Ashan Millewa <ashanmillewa@gmail.com>
2023-12-29 12:37:30 +0530

    intro js and css updated

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './intro.css'
+import bg from '../../assets/image.png' 
+import { FaBagShopping } from "react-icons/fa6";
+import {Link} from 'react-scroll'
 
 const intro = () => {
   return (
-    <div>intro</div>
+    <section id='intro'>
+        <div className='introContent'>
+                <span className='hello'>Hello,</span>
+                <span className='introText'>I'm <span className='introName'>Ashan</span><br/>Website Designer</span>
+                <p className='introPara'>I am a skilled web designer with experience in creating view</p>
+                <Link><button className='btn'><FaBagShopping className='btnIcon'/>Hire Me</button></Link>
+            </div>          
+            <img src={bg} alt='Profile' className='bg'/>
+    </section>
   )
 }
 
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './navbar.css'
 import logo from '../../assets/logo.png'
+import { SlSpeech } from "react-icons/sl";
 import {Link} from 'react-scroll';
 
 const Navbar = () => {
@@ -8,13 +9,14 @@ const Navbar = () => {
     <nav className='navbar'>
       <img src={logo} alt='logo'  className='logo'/>
       <div className='desktopMenu'>
+        
           <Link className='desktopMenuItemList'>Home</Link>
           <Link className='desktopMenuItemList'>About</Link>
           <Link className='desktopMenuItemList'>Portfolio</Link>
           <Link className='desktopMenuItemList'>Clients</Link>
       </div>
       <button className='desktopMenuBtn'>
-        <img src='' alt='' className='desktopMenuImg' /> Contact Me </button>
+      <SlSpeech className='desktopMenuIcon' /> Contact Me </button>
       </nav>
   )
 }
