c800b82dbd349a15bbbaa1cb2e76f58750fbe761
Ashan Millewa <ashanmillewa@gmail.com>
2024-01-03 10:22:02 +0530

    done footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+const footer = () => {
+  return (
+    <footer className='footer'>
+        copyright &#169; 2024 AshanMillewa. All right received.
+    </footer>
+  );
+}
+
+export default footer
